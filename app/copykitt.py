48ca5e964b49a4c773c350299254517559e03de3
Test <wassim.benaissa@hotmail.com>
2022-11-13 00:16:20 +0100

    OK

--- app/copykitt.py
+++ app/copykitt.py
@@ -1,6 +1,10 @@
 import os
 import openai
 import argparse
+import re
+
+
+MAX_INPUT_LENGTH = 12
 
 def main():
     print('Running copy Kitt!')
@@ -9,17 +13,47 @@ def main():
     args = parser.parse_args()
     user_input = args.input
     print(f"User input: {user_input}")
-    generate_branding_snippet(user_input)
+    if validate_length(user_input):
+        generate_branding_snippet(user_input)
+        generate_keywords(user_input)
+    else:
+        raise ValueError(
+            f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is {len(user_input)}"
+        )
     pass
 
-
-
-def generate_branding_snippet (prompt: str)
+def generate_branding_snippet (prompt: str) -> str:
+    # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}"
     response = openai.Completion.create(model="text-davinci-002", prompt=enriched_prompt, temperature=0, max_tokens=32)
-    print(response)
+    #Extract output and strip text
+    branding_text=response["choices"][0]["text"].strip()
+    last_character = branding_text[-1]
+    #Add ... to truncated statements
+    if last_character not in {".", "!", "?"}:
+        branding_text += "..."
+    print(f'branding text: {branding_text}')
+    return branding_text
+
+def validate_length(prompt: str) -> bool:
+    return len(prompt) <= MAX_INPUT_LENGTH
+
+def generate_keywords(prompt: str) -> list[str]:
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate related branding keywords for {prompt}"
+    response = openai.Completion.create(model="text-davinci-002", prompt=enriched_prompt, temperature=0, max_tokens=32)
+    #Extract output and strip text
+    keywords_text=response["choices"][0]["text"].strip()
+    keywords_array = re.split("\n|,|;",keywords_text)
+    keywords_array = [k.lower().strip() for k in keywords_array]
+    keywords_array = [k.lstrip('-') for k in keywords_array]
+    keywords_array = [re.sub(r'[0-9]. ', '',k) for k in keywords_array]
+    keywords_array = [k for k in keywords_array if len(k) > 1]
+    print(f'Keywords: {keywords_array}')
+    return keywords_array
 
 if __name__ == "__main__":
     main()
-# Load your API key from an environment variable or secret management service
+
