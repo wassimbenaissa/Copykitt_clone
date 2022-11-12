1a420f38bc34d416465b94190f256d55c213db51
Test <wassim.benaissa@hotmail.com>
2022-11-12 22:36:59 +0100

    OK

--- app/copykitt.py
+++ app/copykitt.py
@@ -4,20 +4,22 @@ import argparse
 
 def main():
     print('Running copy Kitt!')
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
     print(f"User input: {user_input}")
+    generate_branding_snippet(user_input)
     pass
 
+
+
+def generate_branding_snippet (prompt: str)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate upbeat branding snippet for {prompt}"
+    response = openai.Completion.create(model="text-davinci-002", prompt=enriched_prompt, temperature=0, max_tokens=32)
+    print(response)
 
 if __name__ == "__main__":
     main()
 # Load your API key from an environment variable or secret management service
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-#subject = "Mobility application"
-#prompt = f"Generate upbeat branding snippet for {subject}"
-#response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=32)
-#print(response)
