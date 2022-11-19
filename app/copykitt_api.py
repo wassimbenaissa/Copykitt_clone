828de0a307aaff44394efe942564f78dd99a9014
Test <wassim.benaissa@hotmail.com>
2022-11-19 19:25:04 +0100

    final API edit

--- app/copykitt_api.py
+++ app/copykitt_api.py
@@ -1,22 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from copykitt import generate_branding_snippet, generate_keywords
 
 app = FastAPI()
 
+MAX_INPUT_LENGTH = 12
 
 @app.get("/generate_snippet")
 async def generate_branding_snippet_api(prompt: str):
+    validate_input_length(prompt)
     snippet = generate_branding_snippet(prompt)
     return {"Snippet": snippet, "Keywords": []}
 
 @app.get("/generate_keywords")
 async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
     keywords = generate_keywords(prompt)
     return {"Snippet": None, "Keywords": keywords}
 
 @app.get("/generate_snippet_and_keywords")
 async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
     snippet = generate_branding_snippet(prompt)
     keywords = generate_keywords(prompt)
     return {"Snippet": snippet, "Keywords": keywords}
  
+def validate_input_length (prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code=400, detail=f"Input length is too long. Must be under {MAX_INPUT_LENGTH}")
