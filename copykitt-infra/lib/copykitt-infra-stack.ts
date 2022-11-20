cb839888c0e9ba5aa8b8e1f071e1a36cf4be62a6
Test <wassim.benaissa@hotmail.com>
2022-11-20 20:31:42 +0100

    adding infra files

---
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lamba from 'aws-cdk-lib/aws-lambda'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CopykittInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const apiLambda = new lambda.Function(this, "ApiFunction", {
+    runtime: lambda.Runtime.PYTHON_3_9,
+    code: lamba.Code.fromAsset("../app/"),
+    handler: "copykitt_api.handler"
+    })
+
+  }
+}
