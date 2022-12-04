996b30b911c9a2f4861249656224de5821086417
Test <wassim.benaissa@hotmail.com>
2022-12-04 15:09:51 +0100

    new things comming

--- copykitt-infra/lib/copykitt-infra-stack.ts
+++ copykitt-infra/lib/copykitt-infra-stack.ts
@@ -6,10 +6,15 @@ import * as lambda from 'aws-cdk-lib/aws-lambda'
 export class CopykittInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
+    })
     const apiLambda = new lambda.Function(this, "ApiFunction", {
-    runtime: lambda.Runtime.PYTHON_3_9,
+    runtime: lambda.Runtime.PYTHON_3_7,
     code: lambda.Code.fromAsset("../app/"),
-    handler: "copykitt_api.handler"
+    handler: "copykitt_api.handler",
+    layers: [layer],
     })
 
   }
